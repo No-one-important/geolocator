8ec8fd1f619caa5ce6402d40cdbc9b65f4bd766a
Aaron Huntley <aaron.huntley2007@gmail.com>
2022-06-04 16:00:55 +0100

    Add usage information

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,17 @@ use tokio;
 async fn main() {
     let service = Service::IpApi;
     // get ip from command line
-    let ip = std::env::args().nth(1).unwrap_or("1.1.1.1".to_string());
-
-    match Locator::get(&ip, service).await {
-        Ok(ip) => println!("{} - {} ({})", ip.ip, ip.city, ip.country),
-        Err(error) => println!("Error: {}", error),
-    };
+    let ip = std::env::args().nth(1);
+    match ip {
+        Some(ip) => {
+            match Locator::get(&ip, service).await {
+            Ok(ip) => println!("{} - {} ({})", ip.ip, ip.city, ip.country),
+            Err(error) => println!("Error: {}", error),
+        };
+        }
+        None => {
+            println!("Usage:\n$ geolocator <ip>");
+            return;
+        }
+    }
 }
